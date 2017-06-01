3d70433802939a1eabf859dc17103593dd7c43a3
Andrew <Andrew@10.17.163.185>
2017-05-31 19:40:19 -0600

    31May2017 - MainMenuView.java

---
+++ src/mormontrail/view/MainMenuView.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mormontrail.view;
+
+import java.util.Scanner;
+import mormontrail.MormonTrail;
+
+/**
+ *
+ * @author Andrew
+ */
+public class MainMenuView {
+
+    private String menu;
+    private String promptMessage;
+    
+    public MainMenuView(){
+        this.menu = "\n"
+                  + "\n--------------------------------------" 
+                  + "\n| Main Menu"
+                  + "\n--------------------------------------"
+                  + "\nN - Start new game"
+                  + "\nG - Get and start saved game"
+                  + "\nH - Get help on how to play the game"
+                  + "\nS - Save game"
+                  + "\nQ - Quit"
+                  + "\n--------------------------------------";
+    }
+    
+    public void displayMainMenuView() {
+        boolean done = false;
+        do {
+            String menuOption = this.getMenuOption();
+            if (menuOption.toUpperCase().equals("Q"))
+                return;
+            
+            done = this.doAction(menuOption);
+        } while (!done);
+    }
+
+    private String getMenuOption() {
+        
+        Scanner keyboard = new Scanner(System.in);
+        String value = "";
+        boolean valid = false;
+        
+        while (!valid) {
+            System.out.println("\n" + this.menu);
+            
+            value = keyboard.nextLine();
+            value = value.trim();
+            
+            if (value.length() < 1) {
+                System.out.println("\nInvalid value: value can not be blank");
+                continue;
+            }
+            
+            break;
+            
+        }
+        
+        return value;
+        
+    }
+
+    public boolean doAction(String choice) {
+        
+        choice = choice.toUpperCase();
+        
+        switch (choice) {
+            case "N":
+                this.startNewGame();
+                break;
+            case "G":
+                this.startExistingGame();
+                break;
+            case "H":
+                this.displayHelpMenu();
+                break;
+            case "S":
+                this.saveGame();
+                break;
+            default:
+                System.out.println("\n*** Invalid selection *** Try again");
+                break;
+        
+    }
+        return false;
+        
+    }
+
+    private void startNewGame() {
+        GameControl.createNewGame(MormonTrail.getPlayer());
+        
+        GameMenuView gameMenu = new GameMenuView();
+        gameMenu.displayMenu();
+    }
+
+    private void startExistingGame() {
+        System.out.println("*** startExistingGame function called ***");
+    }
+
+    private void displayHelpMenu() {
+       System.out.println("*** displayHelpMenu function called ***");
+    }
+
+    private void saveGame() {
+        System.out.println("*** startSaveGame function called ***");
+    }
+    
+}
+
