8fc3707f345ec60a4a7dc344f178360d629188de
Andrew <Andrew@10.17.163.185>
2017-07-11 23:53:52 -0600

    7/11/17 - Wagon, from meeting with Brother Jones

--- src/mormontrail/model/Wagon.java
+++ src/mormontrail/model/Wagon.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
  */
 public class Wagon implements Serializable {
     
+    private double minWeight;
     private double maxWeight;
     ArrayList<InventoryItem> inventoryItems;
     private double wagonWeight;
@@ -21,6 +22,7 @@ public class Wagon implements Serializable {
     public Wagon() {
         
         //what else should we include?
+        this.minWeight = 0;
         this.maxWeight = 2525;
         this.wagonWeight = 1000;
         //properties to take from store to wagon - ArrayList<InventoryItem>
